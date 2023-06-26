02a9eae0b8083dcb28505c55deaeddf031844f13
icemanexi <icemanexi@github.com>
2023-06-26 12:01:17 -0400

    messing with gps io in test, comparing imu and magnetometer in test2 	modified:   Sensors/test.py 	modified:   Sensors/test2.py

--- Sensors/test.py
+++ Sensors/test.py
@@ -2,25 +2,29 @@ from gpsModule import ubx, gps_io
 from time import sleep, time
 gpsio = gps_io(input_speed=38400)
 ubxt = ubx.ubx()
-ubxt.protver = 26.0
+ubxt.protver = 33.30 
 ubxt.io_handle = gpsio
 
 def read_ser():
     out = None
     while out == None:
         out = gpsio.ser.sock.recv(8192)
-        print(out, '\n')
+        #print(out, '\n')
     return out
 
-CFG_VALSET = {"class" : 0x06, "id" : 0x8a}
+#CFG_VALSET = {"class" : 0x06, "id" : 0x8a}
 
+
+#ubxt.send_able_binary(1, None)
+#ubxt.send_able_nmea(0, None)
 
 
 #print(ubxt.send_cfg_valget([0x10730001, 0x10730002, 0x10740001, 0x10740002], 1, 0))
 
 #print(ubxt.send_cfg_valset(["CFG-UART1INPROT-UBX,1", "CFG-UART1INPROT-NMEA,0", "CFG-UART1OUTPROT-UBX,1", "CFG-UART1OUTPROT-NMEA,0"]))
-
-for i in range(15):
+#print(ubxt.gps_send(0x27, 0x03, []))
+#input()
+while True:
     ubxt.decode_msg(read_ser())
     #print(ubxt.decode_msg(read_ser()))
 
--- Sensors/test2.py
+++ Sensors/test2.py
@@ -1,13 +1,65 @@
-from gps import ubx, gps_io
-#gpsio = gps_io(input_speed=38400, input_file_name="gps.raw")
-ubxt = ubx.ubx()
+import os
+import numpy as np
+from adafruit_lis3mdl import LIS3MDL, Rate
+from adafruit_extended_bus import ExtendedI2C as I2C
+import matplotlib.pyplot as plt
+from time import sleep, time
+from adafruit_extended_bus import ExtendedI2C as I2C
+import threading
+from math import floor
+from numpy import save
+try:
+    import Sensors.adafruit_bno055 as bno055
+except:
+    import adafruit_bno055 as bno055
 
-with open("gps.raw", "rb") as file:
-    contents = file.read()
+i2c = I2C(1)  # Device is /dev/i2c-1
+mag = LIS3MDL(i2c)
+imu = bno055.BNO055_I2C(i2c)
+current_rate = Rate.RATE_155_HZ
+mag.data_rate = current_rate
+t = []
 
-res = -1
-while res < len(contents)+1:
-    res = ubxt.decode_msg(contents)
-    print(res, len(contents))
-    #print(contents[:res])
-    contents = contents[res:]
+mag1 = []
+mag2 = []
+mag3 = []
+imu1 = []
+imu2 = []
+imu3 = []
+t = []
+
+t0 = time()
+while time() < t0 + 15:
+    # both measure magentic field
+    
+    mag_x, mag_y, mag_z = mag.magnetic
+    imu_x, imu_y, imu_z = imu.magnetic
+    
+    mag1.append(mag_x)
+    mag2.append(mag_y)
+    mag3.append(mag_z)
+    imu1.append(imu_x)
+    imu2.append(imu_y)
+    imu3.append(imu_z)
+    t.append(time())
+    print("mag; imu\n%5.2f, %5.2f, %5.2f\n%5.2f, %5.2f, %5.2f" %(mag_x,mag_y,mag_z,imu_x,imu_y,imu_z))
+
+    # Set sleep time to read value twice per measurement
+    sleep_time = 1 / (Rate.string[current_rate] * 2)
+    sleep(sleep_time)
+
+fig, ax = plt.subplots(3)
+
+ax[0].plot(t, mag1, label="mag1")
+ax[0].plot(t, imu1, label="imu1")
+ax[0].legend()
+
+ax[1].plot(t, mag2, label="mag2")
+ax[1].plot(t, imu2, label="imu2")
+ax[1].legend()
+
+ax[2].plot(t, mag3, label="mag3")
+ax[2].plot(t, imu3, label="imu3")
+ax[2].legend()
+
+plt.show()
